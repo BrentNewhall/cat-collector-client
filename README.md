df7b78ac0af70d9d8af78604f372f0e19ef1ea78
Brent Newhall <brent.newhall@capitalone.com>
2019-02-05 11:45:46 -0500

    Added cats and player. Added keyboard handler. Set up cats in response to server.

--- README.md
+++ README.md
@@ -6,5 +6,11 @@ more cats than the other players.
 This game was written by [Brent P. Newhall](http://brentnewhall.com) as part
 of his weekly video game challenge.
 
+## Resources
+
+The cat sprites are from
+[Cat sprites](https://opengameart.org/content/cat-sprites) on
+[OpenGameArt.org](https://opengameart.org).
+
 This project was bootstrapped with
 [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,34 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import openSocket from 'socket.io-client';
+import KeyboardEventHandler from 'react-keyboard-event-handler';
 
 class App extends Component {
   constructor( props ) {
     super( props );
     this.state = {
       socket: openSocket('http://localhost:1337'),
+      cats: [],
       message: 'Waiting for another player',
+      player: {
+        x: 250,
+        y: 250,
+      }
+    }
+    this.state.socket.on("place cat", (cat) => this.placeCat(cat) );
+    this.sendCollect = this.sendCollect.bind( this );
+    this.placeCat = this.placeCat.bind( this );
+    this.keyPressed = this.keyPressed.bind( this );
+  }
+
+  sendCollect() {
+    //console.log( "Sending collection to server" );
+    this.state.socket.emit( "collect", "cat" );
+  }
+
+  /*
+   * Generates a new cat on the play field.
+   */
+  placeCat(cat) {
+    //console.log( "Placing cat " + cat );
+    this.setState( { cats: [...this.state.cats, cat] } );
+  }
+
+  /*
+   * Handles keyboard events, particularly player control.
+   */
+  keyPressed( key ) {
+    if( key === 'w' ) {
+      this.setState( { player: { x: this.state.player.x, y: this.state.player.y - 10 } } );
+    }
+    else if( key === 'a' ) {
+      this.setState( { player: { x: this.state.player.x - 10, y: this.state.player.y } } );
+    }
+    else if( key === 's' ) {
+      this.setState( { player: { x: this.state.player.x, y: this.state.player.y + 10 } } );
+    }
+    else if( key === 'd' ) {
+      this.setState( { player: { x: this.state.player.x + 10, y: this.state.player.y } } );
     }
   }
+
   render() {
+    const catImages = this.state.cats.map( (cat, index) => {
+      const catStyle = { left: cat.x, top: cat.y };
+      return <img src="/images/catwalkx4.gif" style={catStyle} className="Cat" alt={"cat" + index} key={"cat" + index} />
+    });
+    const playerStyle = { position: 'absolute', left: this.state.player.x, top: this.state.player.y };
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={() => this.sendCollect()}>Collect</button>
           Message: {this.state.message}
         </header>
+        {catImages}
+        <img src="/images/player.png" alt="player" style={playerStyle} />
+        <KeyboardEventHandler
+          handleKeys={['w', 'a', 's', 'd']}
+          onKeyEvent={(key, e) => this.keyPressed(key)} />
+        />
       </div>
     );
   }
